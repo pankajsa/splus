e8de2654a84d0b48a4e1f6ff93874f0b4d42fe26
Pankaj Arora <286749+pankajsa@users.noreply.github.com>
2020-04-05 21:10:40 +0800

    update

--- splus/commands/clientuser_commands.py
+++ splus/commands/clientuser_commands.py
@@ -4,6 +4,7 @@ import click
 from common import *
 from managers import RestMgr
 
+suburl = 'clientUsernames'
 logger = logging.getLogger(__name__)
 
 
@@ -11,9 +12,8 @@ logger = logging.getLogger(__name__)
 def clientuser():
     pass
 
-@clientuser.command(name='create')
+@clientuser.command()
 @click.argument("name")
-@my_global_options
 @click.option('--acl-profile', default='default', show_default=True,
               help='ACL Profile of the Client Username')
 @click.option('--client-profile', default='default', show_default=True,
@@ -26,44 +26,41 @@ def clientuser():
               help='Password for the Client Username')
 @click.option('--subscription-manager/--no-subscription-manager', default=False, show_default=True,
               help='Manage the subscription management capability of the Client Username. This is the ability to manage subscriptions on behalf of other Client Usernames.')
-def clientuser_create(
-                        name,
-                        acl_profile, client_profile, enable, gm_perm_override, password, subscription_manager,
-                        **kwargs):
-    clientuser_upsert(True, name,
-                      acl_profile, client_profile, enable, gm_perm_override, password, subscription_manager,
-                      **kwargs)
-
-
-def clientuser_upsert(is_post, name,
-                      acl_profile, client_profile, enable, gm_perm_override, password, subscription_manager,
-                      **kwargs):
-    try:
-        logging.debug('start')
-
-        dict = {}
-
-        add_if(dict, acl_profile, 'clientUsername', name)
-        add_if(dict, acl_profile, 'aclProfileName', acl_profile)
-        add_if(dict, client_profile, 'clientProfileName', client_profile)
-        add_if(dict, enable, 'enabled')
-        add_if(dict, gm_perm_override, 'guaranteedEndpointPermissionOverrideEnabled')
-        add_if(dict, password, 'password', password)
-        add_if(dict, subscription_manager, 'subscriptionManagerEnabled')
-
-        rest_mgr = RestMgr(kwargs)
-        if is_post:
-            rest_mgr.post('clientUsernames', dict)
-        else:
-            rest_mgr.patch('clientUsernames', name, dict)
-    except Exception as ex:
-        logger.error(f"create Exception: {ex}")
-
-
-
-@clientuser.command(name='update')
-@click.argument("name")
 @my_global_options
+def create(
+    name,
+    acl_profile, client_profile, enable, gm_perm_override, password, subscription_manager,
+    **kwargs):
+    '''Create a new client username'''
+    upsert(True, name,
+        acl_profile, client_profile, enable, gm_perm_override, password, subscription_manager,
+        **kwargs)
+
+
+def upsert(is_post, name,
+    acl_profile, client_profile, enable, gm_perm_override, password, subscription_manager,
+    **kwargs):
+    dict = {}
+
+    add_if(dict, acl_profile, 'clientUsername', name)
+    add_if(dict, acl_profile, 'aclProfileName', acl_profile)
+    add_if(dict, client_profile, 'clientProfileName', client_profile)
+    add_if(dict, enable, 'enabled')
+    add_if(dict, gm_perm_override, 'guaranteedEndpointPermissionOverrideEnabled')
+    add_if(dict, password, 'password', password)
+    add_if(dict, subscription_manager, 'subscriptionManagerEnabled')
+
+    rest_mgr = RestMgr(kwargs)
+    if is_post:
+        res = rest_mgr.post('clientUsernames', dict)
+    else:
+        res = rest_mgr.patch('clientUsernames', name, dict)
+    send_response(res)
+
+
+
+@clientuser.command()
+@click.argument("name")
 @click.option('--acl-profile',
               help='ACL Profile of the Client Username')
 @click.option('--client-profile',
@@ -76,45 +73,40 @@ def clientuser_upsert(is_post, name,
               help='Password for the Client Username')
 @click.option('--subscription-manager/--no-subscription-manager', default=None,
               help='Manage the subscription management capability of the Client Username. This is the ability to manage subscriptions on behalf of other Client Usernames.')
-def clientuser_update(
-        name,
+@my_global_options
+def update(
+    name,
+    acl_profile, client_profile, enable, gm_perm_override, password, subscription_manager,
+    **kwargs):
+    '''Update an existing client username'''
+    upsert(False, name,
         acl_profile, client_profile, enable, gm_perm_override, password, subscription_manager,
-        **kwargs):
-    clientuser_upsert(False, name,
-                      acl_profile, client_profile, enable, gm_perm_override, password, subscription_manager,
-                      **kwargs)
+        **kwargs)
 
-
-
-@clientuser.command(name='show')
+@clientuser.command()
 @click.argument("name")
 @my_global_options
-def clientuser_show( name, **kwargs):
-    try:
-        rest_mgr = RestMgr(kwargs)
-        rest_mgr.get('clientUsernames', name)
-    except Exception as ex:
-        logger.error(f"{ex}")
+def show( name, **kwargs):
+    '''Show details of an existing client username'''
+    rest_mgr = RestMgr(kwargs)
+    res = rest_mgr.get(suburl, name)
+    send_response(res)
 
 
-@clientuser.command(name='list')
-@click.pass_context
+@clientuser.command()
 @my_global_options
-def clientuser_list(ctx, **kwargs):
-    try:
-        rest_mgr = RestMgr(kwargs)
-        rest_mgr.get('clientUsernames')
-    except Exception as ex:
-        logger.error(f"{ex}")
+def list(**kwargs):
+    '''List all client usernames in the Message VPN'''
+    rest_mgr = RestMgr(kwargs)
+    res = rest_mgr.get(suburl)
+    send_response(res)
 
-@clientuser.command(name='remove')
+@clientuser.command()
 @click.argument("name")
 @my_global_options
-def clientuser_remove(name, **kwargs):
-    try:
-        logger.debug(f"remove {name}")
-        rest_mgr = RestMgr(kwargs)
-        rest_mgr.delete('clientUsernames', name)
-    except Exception as ex:
-        logger.error(f"{ex}")
+def remove(name, **kwargs):
+    '''Remove a client username'''
+    rest_mgr = RestMgr(kwargs)
+    res = rest_mgr.delete(suburl, name)
+    send_response(res)
 
